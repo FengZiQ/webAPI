26c64d747a98f5998ca8742c9ccd1879a13e3a65
zach-PC <feng1025352529@qq.com>
2018-02-28 17:51:25 +0800

    update 2018.02.28

--- api_test.py
+++ api_test.py
@@ -7,14 +7,14 @@ import json
 import xlrd
 import time
 
+Pass = "'result': 'p'\n"
+Fail = "'result': 'f'\n"
+
 
 class APITest(object):
 
     def __init__(self, flag=False):
         self.flag = flag
-        self.flag_container = []
-        if 'True' in self.flag_container:
-            self.flag = True
 
     # For test 'post/put/delete' method
     def settings_test(self, method, service, cases_file, sheet_name, hold_time=0):
@@ -45,12 +45,16 @@ class APITest(object):
             # Checking status of request
             if str(result['response']) != '<Response [200]>':
                 self.flag = True
-                self.flag_container.append(str(self.flag))
                 tolog('Fail: HTTP status code is ' + str(result['response']))
                 tolog('Error Message: ' + str(result['text']) + '\n')
                 continue
             else:
                 tolog('Actual: HTTP status code is ' + str(result['response']) + ', request is successful!\n')
+
+        if self.flag:
+            tolog(Fail)
+        else:
+            tolog(Pass)
 
     # For test 'post/put/delete' method
     def settings_test_and_confirm(self, method, service, cases_file, sheet_name, hold_time=0):
@@ -85,13 +89,17 @@ class APITest(object):
                 for key in settings.keys():
                     if settings[key] != response[key]:
                         self.flag = True
-                        self.flag_container.append(str(self.flag))
                         tolog('Fail: Expected ' + key + ' is ' + str(settings[key]) + '. Actuality is ' + str(response[key]))
             else:
                 tolog('Actual: ' + str(result['text']) + '\n')
 
+        if self.flag:
+            tolog(Fail)
+        else:
+            tolog(Pass)
+
     # Only confirm return parameters
-    def message_test(self, service, cases_file, sheet_name):
+    def response_test(self, url_parameters, cases_file, sheet_name):
         # Open cases file
         # data = xlrd.open_workbook('/home/work/zach/clitest/' + data_file_name)
         data = xlrd.open_workbook(cases_file)
@@ -100,7 +108,32 @@ class APITest(object):
         # data for verification
         parameters = [table.cell(0, i).value for i in range(table.ncols)]
 
-        result = server.webapi('get', service)
+        tolog('Expected: ' + str(parameters) + '\n')
+
+        result = server.webapi('get', url_parameters)
+
+        # Verify expected results and actual results
+        if str(result['response']) != '<Response [200]>':
+            self.flag = True
+            tolog('Fail: ' + str(result['text']))
+
+        if self.flag:
+            tolog(Fail)
+        else:
+            tolog(Pass)
+
+    # Confirm return value
+    def response_test_and_confirm(self, url_parameters, cases_file, sheet_name):
+        # Open cases file
+        # data = xlrd.open_workbook('/home/work/zach/clitest/' + data_file_name)
+        data = xlrd.open_workbook(cases_file)
+        table = data.sheet_by_name(sheet_name)
+
+        ed_r = dict((table.cell(0, i).value, table.cell(1, i).value) for i in range(table.ncols))
+
+        tolog('Expected: ' + str(ed_r) + '\n')
+
+        result = server.webapi('get', url_parameters + '/' + str(table.cell(1, 0).value))
 
         # Verify expected results and actual results
         if str(result['response']) != '<Response [200]>':
@@ -108,19 +141,79 @@ class APITest(object):
             tolog('Fail: ' + str(result['text']))
         else:
             check_result = json.loads(result["text"])[0]
-            for key in parameters:
-                if key not in check_result.vaules():
+            for key in ed_r:
+                if key not in check_result.keys():
                     self.flag = True
-                    self.flag_container.append(str(self.flag))
                     tolog('Fail: expected parameter ' + key + ' is not contained at response')
 
-    # Confirm return value
-    def message_test_confirm_values(self, service, cases_file, sheet_name):
+            for key in ed_r:
+                if ed_r[key] != check_result[key]:
+                    self.flag = True
+                    tolog('Fail: Expected value is ' + str(ed_r[key]) + '. Actuality is ' + str(check_result[key]))
+
+            tolog('\nActual: Messages are ' + str(check_result) + '\n')
+
+        if self.flag:
+            tolog(Fail)
+        else:
+            tolog(Pass)
+
+    # For test 'delete' method
+    def delete_test(self, url_parameters, cases_file, sheet_name):
+        # Open cases file
         # data = xlrd.open_workbook('/home/work/zach/clitest/' + data_file_name)
         data = xlrd.open_workbook(cases_file)
         table = data.sheet_by_name(sheet_name)
 
         for i in range(1, table.nrows):
-            check = server.webapi('get', service + '/' + table.cell(i, 0).value)
-            check_result = json.loads(check["text"])[0]
 
+            tolog('Expected: ' + str(table.cell(i, 0).value) + ' should be deleted\n')
+            result = server.webapiurl('delete', url_parameters, str(table.cell(i, 0).value))
+
+            # Checking status of request
+            if str(result['response']) != '<Response [200]>':
+                self.flag = True
+                tolog('Fail: HTTP status code is ' + str(result['response']))
+                tolog('Error Message: ' + str(result['text']) + '\n')
+                continue
+            else:
+                tolog('Actual: HTTP status code is ' + str(result['response']) + ', request is successful!\n')
+
+        if self.flag:
+            tolog(Fail)
+        else:
+            tolog(Pass)
+
+    # For test 'delete' method and confirm response after deleting
+    def delete_test_and_confirm(self, url_parameters, cases_file, sheet_name):
+        # Open cases file
+        # data = xlrd.open_workbook('/home/work/zach/clitest/' + data_file_name)
+        data = xlrd.open_workbook(cases_file)
+        table = data.sheet_by_name(sheet_name)
+
+        for i in range(1, table.nrows):
+
+            tolog('Expected: ' + str(table.cell(i, 0).value) + ' should be deleted\n')
+            result = server.webapiurl('delete', url_parameters, str(table.cell(i, 0).value))
+
+            # Checking status of request
+            if str(result['response']) != '<Response [200]>':
+                self.flag = True
+                tolog('Fail: HTTP status code is ' + str(result['response']))
+                tolog('Error Message: ' + str(result['text']) + '\n')
+                continue
+            else:
+                check_result = server.webapi('get', url_parameters)
+                response_info = json.loads(check_result['text'])
+                if str({table.cell(0, 0).value: table.cell(i, 0).value}) in str(response_info):
+                    self.flag = True
+                    tolog('Fail: parameter ' + str(table.cell(i, 0).value) + ' is not deleted\n')
+                else:
+                    tolog('Actual: parameter ' + str(table.cell(i, 0).value) + ' is deleted')
+                    get_r = str(response_info).replace('},', '\n').replace('[', '\n').replace(']', '')
+                    tolog('And get response: ' + get_r + '\n')
+
+        if self.flag:
+            tolog(Fail)
+        else:
+            tolog(Pass)
--- user.py
+++ user.py
@@ -9,9 +9,31 @@ def add_user():
 
     tool.settings_test('post', 'user', cases_file, 'add_user')
 
-    print tool.flag_container
+
+def get_user():
+
+    tool = APITest()
+
+    tool.response_test('user', cases_file, 'get_user')
+
+
+def get_mgmt_user():
+
+    tool = APITest()
+
+    tool.response_test_and_confirm('user', cases_file, 'get_mgmt_user')
+
+
+def delete_user():
+
+    tool = APITest()
+
+    tool.delete_test_and_confirm('user', cases_file, 'delete_user')
 
 
 if __name__ == "__main__":
 
-    add_user()
+    # add_user()
+    delete_user()
+    # get_user()
+    # get_mgmt_user()
